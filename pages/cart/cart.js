729e3f3d8dbd77c129446d10a7b6b75e8ab2f280
Updating891214 <leiwang@leiwangdeMacBook-Pro.local>
2017-01-11 00:54:09 +0800

    add cart function
    add cart minus & plus function

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -4,7 +4,40 @@ Page({data:{carts: [
             {cid:1031,title:'得力订书机',image:'../../images/stapler.png',num:'3',price:'15.0',sum:'45.0',selected:false},
             {cid:1054,title:'康师傅妙芙蛋糕',image:'../../images/cookie.png',num:'2',price:'15.2',sum:'30.4',selected:false},
             {cid:1063,title:'英雄钢笔',image:'../../images/pen.png',num:'1',price:'122.0',sum:'122.0',selected:true},
-        ]
+        ],
+        minusStatuses:['disabled','disabled','normal','normal','disable']
+    },
+
+    bindMinus:function(e){
+        var index = parseInt(e.currentTarget.dataset.index);
+        var num = this.data.carts[index].num;
+        if(num > 1)
+        {
+            num --;
+        }
+        var minusStatus = num <= 1?'disable':'normal';
+        var carts = this.data.carts;
+        carts[index].num = num;
+        var minusStatuses = this.data.minusStatuses;
+        minusStatuses[index] = minusStatus;
+        this.setData({
+            carts:carts,
+            minusStatuses:minusStatuses
+        });
+    },
+
+    bindPlus:function(e){
+        var index = parseInt(e.currentTarget.dataset.index);
+        var num = this.data.carts[index].num;
+        num++;
+        var minusStatus = num <= 1?'disable':'normal';
+        var carts = this.data.carts;
+        carts[index].num = num;
+        var minusStatuses = this.data.minusStatuses;
+        minusStatuses[index] = minusStatus;
+        this.setData({
+            carts:carts,
+            minusStatuses:minusStatuses
+        });
     }
-  
 })
